c160b20851722e670c45cb7d873aa6f622d645e4
Natalie VanDyke <npcolletti@gmail.com>
2021-03-21 13:54:01 -0700

    Added queries and tables to solve for retirement eligibility discrepancy

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -56,3 +56,34 @@ INTO mentorship_titles
 FROM mentorship_eligibility 
 GROUP BY title 
 ORDER BY mentors 
+
+-- Find ACTUAL number of ELIGIBLE retirees by title (include hire date filter
+-- this time, unlike challenge)
+SELECT e.emp_no,
+		e.first_name, 
+		e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO eligible_by_hire_date_retirement_titles 
+FROM employees as e 
+INNER JOIN titles as t 
+ON e.emp_no = t.emp_no 
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN'1985-01-01' AND '1988-12-31')
+ORDER BY emp_no; 
+
+SELECT DISTINCT ON (emp_no) emp_no, 
+		first_name, 
+		last_name, 
+		title
+INTO eligible_by_hire_date_unique_titles
+FROM eligible_by_hire_date_retirement_titles
+ORDER BY emp_no, to_date DESC; 
+
+SELECT COUNT (title),title
+INTO eligible_by_hire_date_retiring_titles 
+FROM eligible_by_hire_date_unique_titles
+GROUP BY title
+ORDER BY count DESC;
+SELECT * FROM eligible_by_hire_date_retiring_titles
